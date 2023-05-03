40783c399b9fc114004b7aa9e0fe3f4f8f701185
Ermi <manermidem@gmail.com>
2023-05-03 13:42:21 +0300

    For readind articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root 'pages#hello'
-  get '/greeting', to:'custom_pages#hello'
+  root 'articles#index'
+  get '/greeting', to: 'custom_pages#hello'
+
+  # get '/articles', to: 'articles#index'
+  # get '/articles/:id', to: "articles#show"
+
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
